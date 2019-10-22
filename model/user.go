209bcc6cfd8774607263ffece4566b67f84dc6ed
Correia Lopes Dylan <dylan.correia@hotmail.com>
2019-10-22 14:40:38 +0200

    wip

--- model/user.go
+++ model/user.go
@@ -1,19 +1,20 @@
 package model
 
 import (
-	"github.com/jinzhu/gorm"
 	"time"
 )
 
 // User is the representation of a client.
 type User struct {
-	gorm.Model
-	UUID      string `json:"uuid"`
+	//gorm.Model
+	UUID      string `json:"uuid" gorm:"primary_key"`
 	AccessLevel int `json:"access_level"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email string `json:"email"`
 	Password string `json:"password"`
 	DateOfBirth time.Time `json:"date_of_birth"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
 
