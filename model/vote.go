3be74622e0d5c8c69010d11524b80440d2ef2fe7
Amandine <amandine-ripoll@outlook.fr>
2019-11-11 16:07:22 +0100

    add, get, getAll and delete done, have to work on update

--- model/vote.go
+++ model/vote.go
@@ -4,16 +4,16 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
-
+	"github.com/lib/pq"
 )
 
 // Vote is the representation of a client.
 type Vote struct {
 	gorm.Model
-	UUID      string    `json:"uuid" gorm:"primary_key"`
-	Title     string    `json:"title" validate:"required,min=2"`
-	Desc      string    `json:"desc" validate:"required,min=2"`
-	StartDate time.Time `json:"start_date"`
-	EndDate   time.Time `json:"end_date"`
-	UUIDVote  []string `json:"uuid_vote" gorm:"type:text[]"`
+	UUID      string         `json:"uuid" gorm:"primary_key"`
+	Title     string         `json:"title" validate:"required,min=2"`
+	Desc      string         `json:"desc" validate:"required,min=2"`
+	StartDate time.Time      `json:"start_date"`
+	EndDate   time.Time      `json:"end_date"`
+	UUIDVote  pq.StringArray `json:"uuid_votes" gorm:"type:text[]"`
 }
